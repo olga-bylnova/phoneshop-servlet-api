1bc0dfc33014651b622d1c4a686baacdd7cd9396
olga-bylnova <olga.bylnova@yandex.by>
2023-05-05 00:37:39 +0300

    Task 2.1: Implement product search

--- src/main/java/com/es/phoneshop/web/ProductListPageServlet.java
+++ src/main/java/com/es/phoneshop/web/ProductListPageServlet.java
@@ -1,31 +1,40 @@
 package com.es.phoneshop.web;
 
-import com.es.phoneshop.model.product.ArrayListProductDao;
-import com.es.phoneshop.model.product.Product;
+import com.es.phoneshop.model.product.*;
 
-import com.es.phoneshop.model.product.ProductDao;
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Currency;
 import java.util.List;
+import java.util.Optional;
 
 public class ProductListPageServlet extends HttpServlet {
     private ProductDao productDao;
+
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
-        productDao = new ArrayListProductDao();
+        productDao = ArrayListProductDao.getInstance();
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setAttribute("products", productDao.findProducts());
+        String query = request.getParameter("query");
+
+        String sortFieldParam = request.getParameter("sort");
+        String sortOrderParam = request.getParameter("order");
+
+        request.setAttribute("products", productDao.findProducts(query,
+                Optional.ofNullable(sortFieldParam).map(SortField::valueOf).orElse(null),
+                Optional.ofNullable(sortOrderParam).map(SortOrder::valueOf).orElse(null)
+        ));
         request.getRequestDispatcher("/WEB-INF/pages/productList.jsp").forward(request, response);
     }
 }
