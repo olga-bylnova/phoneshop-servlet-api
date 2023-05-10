97c96f1c80011b3ee429b1a15c54d888851b0f9e
olga-bylnova <olga.bylnova@yandex.by>
2023-05-10 14:56:35 +0300

    Task 3.3: HttpSessionCartService; add consideration of existing product quantity

---
+++ src/main/java/com/es/phoneshop/service/CartService.java
@@ -0,0 +1,10 @@
+package com.es.phoneshop.service;
+
+import com.es.phoneshop.exception.OutOfStockException;
+import com.es.phoneshop.model.cart.Cart;
+import jakarta.servlet.http.HttpServletRequest;
+
+public interface CartService {
+    Cart getCart(HttpServletRequest request);
+    void add(Cart cart, Long productId, int quantity) throws OutOfStockException;
+}
